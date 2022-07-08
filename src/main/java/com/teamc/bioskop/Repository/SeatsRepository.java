28822aa52de2ebaea2f6b8f2a37afb2aba92574e
ZainalAbiddin <salahuddinade@gmail.com>
2022-07-08 12:14:41 +0700

    tampilan seat

--- src/main/java/com/teamc/bioskop/Repository/SeatsRepository.java
+++ src/main/java/com/teamc/bioskop/Repository/SeatsRepository.java
@@ -10,4 +10,8 @@ import java.util.*;
 public interface SeatsRepository extends JpaRepository<Seats, Long> {
     @Query(value = "select * from seats s where is_available =?1", nativeQuery = true)
     public List<Seats> getSeatAvailable(Integer isAvailable);
+
+    @Query("SELECT s2 FROM Seats s2 WHERE CONCAT(s2.seatId, ' ', s2.seatNumber, ' ', s2.studioName, ' ', s2.isAvailable, ' ') LIKE %?1%")
+    public List<Seats> search(String keyword);
+
 }
