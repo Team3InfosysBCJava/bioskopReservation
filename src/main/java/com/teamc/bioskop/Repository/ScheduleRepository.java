7fcefae07ce16e40dfefd60e375acabe1037497f
Bayu Aji <bayuaajipamungkas@gmail.com>
2022-07-11 01:32:47 +0700

    sorting and paging backend Done

--- src/main/java/com/teamc/bioskop/Repository/ScheduleRepository.java
+++ src/main/java/com/teamc/bioskop/Repository/ScheduleRepository.java
@@ -3,6 +3,7 @@ package com.teamc.bioskop.Repository;
 import com.teamc.bioskop.Model.Schedule;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -16,7 +17,7 @@ public interface ScheduleRepository extends JpaRepository<Schedule, Integer> {
     @Query("Select s from Schedule s where s.films.name like %:name%")
     public List<Schedule> getScheduleFilmsNameLike (@Param("name")String name);
 
-    @Query("Select s from Schedule s where s.films.name like %:name%")
+    @Query("Select s from Schedule s where s.films.name like %:name% ORDER BY s.films.name ASC")
     public Page<Schedule> searchByName(@Param("name")String name, Pageable pageable);
 
 }
